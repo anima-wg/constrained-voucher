0c4aa5aef1c71ca361894d2648bc5e3d93a26491
Esko Dijk <EskoDijk@users.noreply.github.com>
2022-12-13 08:57:46 +0100

    Update Appendix C COSE examples - based on interop code (#249)
    * Added scripts and key material to generate Appendix C examples.
    
    * More Appendix C COSE examples added; Makefile for COSE examples simplified; minor issues fixed; formatting updates to stay within 72 char limit; textual form of PVR RVR Voucher and keys added.
    
    * constrained-voucher.txt now set again to text of pr-update-cose-examples branch - to be able to see what changed, in the PR
    
    * use native line endings
    
    * Script updated for OpenSSL 3.x use: explicit key for SKID = none for Pledge added. Removed unneeded parts of AKID for MASA CA. All examples regenerated.
    
    Co-authored-by: Michael Richardson <mcr@sandelman.ca>

---
+++ examples/script-cose-examples/create-cert-DomainCA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Create new cert for: Domain CA
+
+# days certificate is valid
+VALIDITY=3650
+
+NAME=domain_ca
+
+# create an example Domain CA of company.
+openssl req -new -key keys/privkey_domain_ca.pem -out $NAME.csr -subj "/CN=Custom-ER Global CA/OU=IT/O=Custom-ER, Inc./L=San Jose/ST=CA/C=US"
+openssl x509 -set_serial 0x2AEA0413A42DC1CE -extfile x509v3.ext -extensions domain_ca_ext -req -in $NAME.csr -signkey keys/privkey_domain_ca.pem -out output/$NAME.pem -days $VALIDITY -sha256
+
+# delete temp files
+rm -f $NAME.csr
+
+# convert to .der format
+openssl x509 -in output/$NAME.pem -inform PEM -out output/$NAME.der -outform DER
+
