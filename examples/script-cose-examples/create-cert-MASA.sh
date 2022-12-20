b92bf0e5d3b745d3461bf7defbdaefe99548abb2
Esko Dijk <esko.dijk@iotconsultancy.nl>
2022-12-20 11:11:43 +0100

    Include actual example generation scripts into Appendix D; Shortened all relevant scripts to <= 70 chars line length for inclusion in the text; Makefile updates to point to correct example files.

--- examples/script-cose-examples/create-cert-MASA.sh
+++ examples/script-cose-examples/create-cert-MASA.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# File: create-cert-MASA.sh
 # Create new cert for: MASA CA, self-signed CA certificate
 
 # days certificate is valid
@@ -7,14 +8,19 @@ VALIDITY=3650
 NAME=masa_ca
 
 # create csr
-openssl req -new -key keys/privkey_masa_ca.pem -out $NAME.csr -subj "/CN=masa.stok.nl/O=vanderstok/L=Helmond/C=NL"
+openssl req -new -key keys/privkey_masa_ca.pem -out $NAME.csr \
+            -subj "/CN=masa.stok.nl/O=vanderstok/L=Helmond/C=NL"
 
 # sign csr
 mkdir output >& /dev/null
-openssl x509 -set_serial 0xE39CDA17E1386A0A  -extfile x509v3.ext -extensions masa_ca_ext -req -in $NAME.csr -signkey keys/privkey_masa_ca.pem -out output/$NAME.pem -days $VALIDITY -sha256
+openssl x509 -set_serial 0xE39CDA17E1386A0A  -extfile x509v3.ext \
+ -extensions masa_ca_ext -req -in $NAME.csr \
+ -signkey keys/privkey_masa_ca.pem -out output/$NAME.pem \
+ -days $VALIDITY -sha256
 
 # delete temp files
 rm -f $NAME.csr
 
 # convert to .der format
-openssl x509 -in output/$NAME.pem -inform PEM -out output/$NAME.der -outform DER
+openssl x509 -in output/$NAME.pem -inform PEM -out output/$NAME.der \
+             -outform DER
--- examples/script-cose-examples/create-cert-Pledge.sh
+++ examples/script-cose-examples/create-cert-Pledge.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
+# File: create-cert-Pledge.sh
 # Create new cert for: Pledge IDevID
 
-# days certificate is valid - try to get close to the 802.1AR specified 9999-12-31 end date.
+# days certificate is valid - try to get close to the 802.1AR 
+# specified 9999-12-31 end date.
 SECONDS1=`date +%s` # time now
-SECONDS2=`date --date="9999-12-31 23:59:59Z" +%s`   # target end time
+SECONDS2=`date --date="9999-12-31 23:59:59Z" +%s` # target end time
 let VALIDITY="(${SECONDS2}-${SECONDS1})/(24*3600)"
 echo "Using validity param -days ${VALIDITY}"
 
 NAME=pledge
 
 # create csr for device
-# conform to 802.1AR guidelines, using only CN + serialNumber when manufacturer is already present as CA.
+# conform to 802.1AR guidelines, using only CN + serialNumber when 
+# manufacturer is already present as CA.
 # CN is not even mandatory, but just good practice.
-openssl req -new -key keys/privkey_pledge.pem -out $NAME.csr -subj "/CN=Stok IoT sensor Y-42/serialNumber=JADA123456789"
+openssl req -new -key keys/privkey_pledge.pem -out $NAME.csr -subj \
+             "/CN=Stok IoT sensor Y-42/serialNumber=JADA123456789"
 
 # sign csr
-openssl x509 -set_serial 32429 -CAform PEM -CA output/masa_ca.pem -CAkey keys/privkey_masa_ca.pem -extfile x509v3.ext -extensions pledge_ext -req -in $NAME.csr -out output/$NAME.pem -days $VALIDITY -sha256
+openssl x509 -set_serial 32429 -CAform PEM -CA output/masa_ca.pem \
+  -CAkey keys/privkey_masa_ca.pem -extfile x509v3.ext -extensions \
+  pledge_ext -req -in $NAME.csr -out output/$NAME.pem \
+  -days $VALIDITY -sha256
 
 # delete temp files
 rm -f $NAME.csr
 
 # convert to .der format
-openssl x509 -in output/$NAME.pem -inform PEM -out output/$NAME.der -outform DER
-
+openssl x509 -in output/$NAME.pem -inform PEM -out output/$NAME.der \
+             -outform DER
